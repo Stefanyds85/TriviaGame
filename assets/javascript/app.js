10f3bd1a5a55319d303248e09e5a898ed87d2553
Stefanyds85 <beautyxyzu4@aol.com>
2017-11-25 11:03:10 -0500

    trivia game

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+var qAOptions = {
+    
+            question1: {
+                question: "What is the symbol of the house of Stark?",
+                answers: ["A Direwolf", "", "", ""],
+            },
+    
+            question2: {
+                question: "In The game of Thrones, where is the 'Iron Throne' located?",
+                answers: ["", "", "King’s Landing", ""],
+            },
+    
+            question3: {
+                question: "In the Game of Thrones, Jamie Lannister is known as the Kingslayer. Which King did he kill?",
+                answers: ["", "", "", "Aerys II"]
+            },
+        
+            question4: {
+                question: "In the game of Thrones, it is revealed that Robert is not the father of Joffrey. Who is?",
+                answers: ["", "", "", "Jamie Lannister"]
+            },
+
+            question5: {
+                question: "The house Targaryen is virtually destroyed before the start of The Game of Thrones. Where    do the last members of the family take refuge",
+                answers: ["", "The Free Cities", "", ""]
+            },
+
+            question6: {
+                question: "In the novel, The Game of Thrones, Lady Catelyn married into the Stark family. Which house was she born into?",
+                answers: ["", "", "", "House Tully"]
+            },
+
+            question7: {
+                question: "How many 'Great Families' are in The Game of Thrones?",
+                answers: ["4", "7", "12", "9"]
+            },
+
+            question8: {
+                question: "How many children do Eddard and Catelyn Stark have in The Game of Thrones?",
+                answers: ["6", "3", "8", "5"]
+            },
+
+            question9: {
+                question: "Who wrote The Game of Thrones?",
+                answers: ["George R.R. Martin", "", "", ""]
+            },
+
+            question10: {
+                question: "In The Game of Thrones, how many dragons does Daenerys have?"                ,
+                answers: ["", "", "3", ""]
+            },
+    
+        }
+
+     
